e3744919ebfe0f499d09b2f792ba21b272f7ba6e
niviusha <nivethavadivelu@gmail.com>
2015-11-21 17:05:13 -0600

    Forms elements

---
+++ forms/headerTags.php
@@ -0,0 +1,9 @@
+<!-- <meta http-equiv="refresh" content="3"> -->
+<title>FREEMIUM</title>
+
+<!--Include the bootstrap stylesheet-->
+<link rel="stylesheet" type="text/css" href="../bootstrap/css/bootstrap.min.css">
+<!--Include the jquery api-->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<!--Include the bootstrap javascript-->
+<script src="../bootstrap/js/bootstrap.min.js"></script>
