40c8cd80cd575d4fdef91e794b5150db9f2a95c9
Prasanth Krishnan <knp281192@gmail.com>
2015-11-21 18:16:24 -0600

    Add curret Location

--- index.php
+++ index.php
@@ -7,8 +7,42 @@
 			height: 400px;
 		}
 	</style>
-	<script src="https://maps.googleapis.com/maps/api/js"></script>
-	<script>
+</head>
+<body>
+	<div id="map"></div>
+</body>
+<script>
+		function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+		  infoWindow.setPosition(pos);
+		  infoWindow.setContent(browserHasGeolocation ?
+		                        'Error: The Geolocation service failed.' :
+		                        'Error: Your browser doesn\'t support geolocation.');
+		}
+
+		function getCurrentPosition (map) {
+			var pos = {};
+				// Try HTML5 geolocation.
+		  if (navigator.geolocation) {
+		  	navigator.geolocation.getCurrentPosition(function(position) {
+		  		 currentPosition = {
+		  			lat: position.coords.latitude,
+		  			lng: position.coords.longitude
+		  		};
+				var infoWindow = new google.maps.InfoWindow({map: map});
+				infoWindow.setPosition(currentPosition);
+      	infoWindow.setContent('Your Location.');
+				map.setCenter(currentPosition);
+		  		
+		  	}, function() {
+		  		handleLocationError(true, infoWindow, map.getCenter());
+		  	});
+		  } else {
+		    // Browser doesn't support Geolocation
+		    handleLocationError(false, infoWindow, map.getCenter());
+		  }
+		  return pos;
+		}
+
 		function initMap() {
 			var mapCanvas = document.getElementById('map');
 			var mapOptions = {
@@ -20,15 +54,12 @@
 				scaleControl: true,
 				streetViewControl: true,
 				rotateControl: true
-			}
-			var map = new google.maps.Map(mapCanvas, mapOptions)
-		}
-	</script>
-</head>
-<body>
-	<div id="map"></div>
-</body>
+			};
+			var map = new google.maps.Map(mapCanvas, mapOptions);
+			getCurrentPosition(map);
+	}
+</script>
 <script async defer
-src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA1MgYE-d_EX4Jq0b-D-eB2px1NTQxYXW0&callback=initMap">
+	src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA1MgYE-d_EX4Jq0b-D-eB2px1NTQxYXW0&callback=initMap">
 </script>
 </html>
