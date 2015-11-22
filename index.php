441e98f0ff77da96c6d7668e8cb932cf0cdf10c6
Prasanth Krishnan <knp281192@gmail.com>
2015-11-21 20:21:33 -0600

    Adds markers and splits the files into seprate js

--- index.php
+++ index.php
@@ -11,42 +11,11 @@
 <body>
 	<div id="map"></div>
 </body>
+<script type="text/javascript" src="gmap_utils.js"></script>
 <script>
-		function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-		  infoWindow.setPosition(pos);
-		  infoWindow.setContent(browserHasGeolocation ?
-		                        'Error: The Geolocation service failed.' :
-		                        'Error: Your browser doesn\'t support geolocation.');
-		}
-
-		function getCurrentPosition (map) {
-			var pos = {};
-				// Try HTML5 geolocation.
-		  if (navigator.geolocation) {
-		  	navigator.geolocation.getCurrentPosition(function(position) {
-		  		 currentPosition = {
-		  			lat: position.coords.latitude,
-		  			lng: position.coords.longitude
-		  		};
-				var infoWindow = new google.maps.InfoWindow({map: map});
-				infoWindow.setPosition(currentPosition);
-      	infoWindow.setContent('Your Location.');
-				map.setCenter(currentPosition);
-		  		
-		  	}, function() {
-		  		handleLocationError(true, infoWindow, map.getCenter());
-		  	});
-		  } else {
-		    // Browser doesn't support Geolocation
-		    handleLocationError(false, infoWindow, map.getCenter());
-		  }
-		  return pos;
-		}
-
 		function initMap() {
 			var mapCanvas = document.getElementById('map');
 			var mapOptions = {
-				center: new google.maps.LatLng(44.5403, -78.5463),
 				zoom: 8,
 				mapTypeId: google.maps.MapTypeId.ROADMAP,
 				zoomControl: true,
@@ -56,10 +25,10 @@
 				rotateControl: true
 			};
 			var map = new google.maps.Map(mapCanvas, mapOptions);
-			getCurrentPosition(map);
-	}
-</script>
-<script async defer
+			setCurrentPosition(map);
+		}
+	</script>
+	<script async defer
 	src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA1MgYE-d_EX4Jq0b-D-eB2px1NTQxYXW0&callback=initMap">
 </script>
 </html>
